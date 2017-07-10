8df227b0ce9cb94282da549829264b8fb035b087
Brandon Walden <brandonshanewalden@gmail.com>
2017-07-10 16:42:01 -0400

    completed exercise

--- main.js
+++ main.js
@@ -13,8 +13,16 @@ function question1() {
     sum += data[i].price;
   }
   console.log('The average price is $' + (sum / data.length).toFixed(2));
-
 }
+
+//forEach method
+// function question1() {
+//   let sum = 0;
+//   data.forEach(function(element) {
+//     sum = sum + element.price;
+//   });
+//   console.log("The average price is $", (sum / data.length).toFixed(2));
+// }
 
 // 2: Show me how to get an array of items that cost between $14.00 and $18.00 USD
 function question2() {
@@ -26,6 +34,13 @@ function question2() {
   }
   console.log(items);
 }
+//forEach method
+// function question2() {
+//   data.forEach(function(element) {
+//     if (element.price <= 18 && element.price >= 14)
+//       console.log(element.title);
+//   });
+// }
 
 
 // 3: Which item has a "GBP" currency code? Display it's name and price.
@@ -36,24 +51,44 @@ function question3() {
       array.push(data[i].title, data[i].price)
     }
   }
-    console.log(array);
-
+  console.log(array);
 }
+//forEach method
+// function question3() {
+//   data.forEach(function(element) {
+//     if (element.currency_code === "GBP")
+//       console.log(element.title, element.price, "pounds");
+//   });
+// }
+
 // 4: Display a list of all items who are made of wood.
 function question4() {
-  // Answer:
+  data.forEach(function(element) {
+    element.materials.forEach(function(items) {
+      if (items === "wood")
+      console.log(element.title, "is made of Wood.");
+    });
+  });
 }
 
 
 // 5: Which items are made of eight or more materials?
 //    Display the name, number of items and the items it is made of.
 function question5() {
-  // Answer:
+  data.forEach(function(element) {
+    if (element.materials.length >= 8)
+      console.log(element.title, element.items, element.materials);
+  });
 }
 
 
 // 6: How many items were made by their sellers?
-// Answer:
 function question6() {
-  // Answer:
+  let madeBy = 0;
+  data.forEach(function(element) {
+    if (element.who_made === "i_did") {
+      madeBy++
+    }
+  })
+  console.log(madeBy + " items were made by their sellers.")
 }
